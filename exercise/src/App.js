101235c5f6427706135d1a50803430168970f78e
amanhidayat <amanhidayat39@gmail.com>
2023-09-27 09:08:08 +0700

    Latihan5

--- exercise/src/App.js
+++ exercise/src/App.js
@@ -1,12 +1,22 @@
+import { Routes, Route } from "react-router-dom"
 
+import { useState } from "react"
+
+import MainDisplay from "./components/mainDisplay";
+import ChatDisplay from "./components/chatDisplay";
 
 function App() {
-    return (
-      <div>
-        <h1>Hallo</h1>
-        <Count />
-      </div>
-    );
-  }
+  const [message, setMessage] = useState("")
+
+  return (
+    <div>
+      <Routes>
+        <Route path="/" element={<MainDisplay setMessage={setMessage} />} />
+        <Route path="/chatDisplay" element={<ChatDisplay message={message} />} />
+      </Routes>
+    </div>
+  );
+}
 
 export default App;
+
