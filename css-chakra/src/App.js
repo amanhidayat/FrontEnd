5d69ef967cc7a90b3dfed1926bf0eb5688058774
amanhidayat <amanhidayat39@gmail.com>
2023-09-27 13:42:48 +0700

    day-6

---
+++ css-chakra/src/App.js
@@ -0,0 +1,11 @@
+import { Flex } from "@chakra-ui/react";
+import Home from "./pages/home"
+
+function App() {
+  return ( 
+   <Home /> 
+    
+  );
+}
+
+export default App;
---
+++ css-framework/src/App.js
@@ -0,0 +1,12 @@
+import HOME from "./pages/home";
+
+
+function App() {
+  return (
+    <div >
+      <HOME />
+    </div>
+  );
+}
+
+export default App;
