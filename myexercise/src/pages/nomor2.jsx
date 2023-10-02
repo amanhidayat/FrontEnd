bdb4f0fd009b68e466dad06fd0970ae91409121e
amanhidayat <amanhidayat39@gmail.com>
2023-10-02 11:47:04 +0700

    day-7

--- myexercise/src/pages/nomor2.jsx
+++ myexercise/src/pages/nomor2.jsx
@@ -1,17 +1,7 @@
-import React, { useState, useEffect } from "react";
-import { AddIcon, MinusIcon } from "@chakra-ui/icons";
-import { IconNam } from '@chakra-ui/icons';
+import React, { useState, useEffect } from 'react';
+import { ChakraProvider, VStack, Box, Text, HStack, Button } from "@chakra-ui/react";
 
-import {
-  ChakraProvider,
-  Box,
-  Button,
-  Text,
-  VStack,
-  HStack,
-} from "@chakra-ui/react";
-
-function nomor2() {
+function Nomor2() {
   const [isRunning, setIsRunning] = useState(false);
   const [time, setTime] = useState(0);
 
@@ -50,35 +40,26 @@ function nomor2() {
 
   return (
     <ChakraProvider>
-      <VStack spacing={4} align="center" borderRadius="px">
-      <Box border={"30px"} borderRadius="10px" width={"60vh"}  height="180px" backgroundColor={"black"} alignItems="center">
-        <Text fontSize="6xl" color="white" display={"flex"} justifyContent={"center"} fontWeight={"bold"}>{formatTime(time)}</Text>
-        <HStack spacing={4} justifyContent={"center"} m={5}>
-          <Button
-            colorScheme="green"
-            onClick={startTimer}
-          >
-            start
-          </Button>
-          <Button
-            colorScheme="red"
-            onClick={stopTimer}
-          >
-            Stop
-          </Button>
-          <Button
-            colorScheme="yellow"
-            onClick={resetTimer}
-          >
-            Reset
-          </Button>
-        </HStack>
+      <VStack spacing={4} align="center">
+        <Box borderRadius="10px" width="60vh" height="180px" backgroundColor="black" alignItems="center" justifyContent="center">
+          <Text fontSize="6xl" color="white" fontWeight="bold">
+            {formatTime(time)}
+          </Text>
+          <HStack spacing={4} justifyContent="center" m={5}>
+            <Button colorScheme="green" onClick={startTimer}>
+              Start
+            </Button>
+            <Button colorScheme="red" onClick={stopTimer}>
+              Stop
+            </Button>
+            <Button colorScheme="yellow" onClick={resetTimer}>
+              Reset
+            </Button>
+          </HStack>
         </Box>
       </VStack>
-      
     </ChakraProvider>
-    
   );
 }
 
-export default nomor2;
+export default Nomor2;
