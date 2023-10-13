4f3af7cc3e409d27a0cea7b38e78957d20f8e594
amanhidayat <amanhidayat39@gmail.com>
2023-10-13 09:28:37 +0700

    day-15

---
+++ materi-15/src/pages/client-jokes.js
@@ -0,0 +1,34 @@
+const { useState, useEffect } = require("react");
+
+const ClientJokes = () => {
+    const [data, setData] = useState([]);
+    
+    const getData = async () => {
+        try {
+            const response = await fetch("https://v2.jokeapi.dev/joke/Programming?type=twopart&idRange=0-10&amount=5");
+            const { jokes } = await response.json();
+            setData(jokes);
+        } catch (error) {
+            console.log(error);
+        }
+    };
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return (
+        <main>
+            <h1>Here are some Jokes!</h1>
+            <ul>
+                {data.map((joke) => (
+                    <li key={joke.id}>
+                        {joke.setup} - {joke.delivery}
+                    </li>
+                ))}
+            </ul>
+        </main>
+    );
+};
+
+export default ClientJokes;
